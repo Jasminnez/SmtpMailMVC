c7e2589383e4c11ee7088e7cec33492a0125d2a2
Jasmin <Jasmin@DESKTOP-HLFIHK2>
2021-04-09 00:17:35 +0200

    Add project files.

---
+++ SmtpMailMVC/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmtpMailMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SmtpMailMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+            private readonly ILogger<HomeController> _logger;
+
+            public HomeController(ILogger<HomeController> logger)
+            {
+                _logger = logger;
+            }
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            public IActionResult Privacy()
+            {
+                return View();
+            }
+            [HttpGet]
+            public IActionResult ContactUs()
+            {
+                return View();
+            }
+            [HttpPost]
+            public IActionResult ContactUs(SendMailTo sendMailTo)
+            {
+                if (!ModelState.IsValid)
+                    return View();
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.From = new MailAddress("jasminsmtp@gmail.com");
+                    mail.To.Add("jasminneziric@gmail.com");
+
+                    mail.Subject = sendMailTo.Subject;
+
+                    mail.IsBodyHtml = true;
+
+                    string content = "Name : " + sendMailTo.Name;
+                    content += "<br/>Message : " + sendMailTo.Message;
+
+                    mail.Body = content;
+
+
+                    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+                    NetworkCredential networkCredential = new NetworkCredential("jasminsmtp@gmail.com", "pw");//Add password
+                    //smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = networkCredential;
+                    //smtpClient.Port = 587;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Send(mail);
+
+                    ViewBag.Message = "Mail Send";
+
+                    ModelState.Clear();
+
+
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = ex.Message.ToString();
+                }
+                return View();
+            }
+
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+};
---
+++ SmtpMailMVC/Models/SendMailTo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmtpMailMVC.Models
+{
+    public class SendMailTo
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
